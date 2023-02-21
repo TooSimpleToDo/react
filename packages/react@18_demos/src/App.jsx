fa039dc637f6771daa331fefa6a468166326592e
daidaibenben <wxungang@163.com>
2023-02-21 18:34:26 +0800

    init

---
+++ packages/react@18_demos/src/App.jsx
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+
+import ReactClass from './class/reactClass';
+import ReactHooks from './hooks/reactHooks';
+import ReactRef from './ref/reactRef';
+
+const displayName = 'app';
+
+class app extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      reactClass: true,
+      reactHooks: true,
+      reactRef: true
+    };
+  }
+
+  static displayName = displayName;
+
+  componentDidMount() {
+    console.log(`componentDidMount====> ${displayName} `);
+  }
+
+  render() {
+    console.log(`render======>${displayName}`, JSON.stringify(this.props), JSON.stringify(this.state));
+    return (
+      <div className="App" style={{ margin: '20px 5px' }}>
+        {this.state.reactClass && <ReactClass />}
+        {this.state.reactHooks && <ReactHooks />}
+        {this.state.reactRef && <ReactRef />}
+      </div>
+    );
+  }
+}
+
+export default app;
---
+++ packages/react_without_build/like_button.js
@@ -0,0 +1,52 @@
+'use strict';
+
+const createElement = React.createElement;
+
+class LikeButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { liked: false };
+  }
+
+  render() {
+    if (this.state.liked) {
+      return 'You liked this.(class)';
+    }
+
+    return createElement(
+      'button',
+      { onClick: () => this.setState({ liked: true }) },
+      'Like(class)'
+    );
+  }
+}
+console.log(
+  LikeButton,
+  createElement(LikeButton),
+  createElement('button', { onClick: () => {} }, 'Like')
+);
+
+ReactDOM.render(
+  createElement(LikeButton),
+  document.getElementById('class_button_container')
+);
+
+function reactButton() {
+  const [liked, updateLiked] = React.useState(false);
+
+  if (liked) {
+    return 'You liked this.(function)';
+  }
+
+  return createElement(
+    'button',
+    { onClick: () => updateLiked(true) },
+    'Like(function)'
+  );
+}
+
+ReactDOM.render(
+  createElement(reactButton),
+  document.getElementById('function_button_container')
+);
+
